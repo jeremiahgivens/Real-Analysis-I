cb9a19c83ae9f858645e0552074864808d2f5da5
Jeremiah Givens <jeremiahgivens149@gmail.com>
2022-08-21 21:00:21 -0500

    Worked further on chapter 3 practice problems 1.

--- PracticeProblems/Chapter3PracticeProblems1/Problem1Test.py
+++ PracticeProblems/Chapter3PracticeProblems1/Problem1Test.py
@@ -10,7 +10,7 @@ eps = 0.5
 area = (a1 - b1)*(a2 - b2)
 
 A = (a1 - b1) + (a2 - b2)
-x = (1/4)*(A + math.sqrt(A**2 + 4*eps))
+x = (1/4)*(A - math.sqrt(A**2 + 4*eps))
 
 area2 = (a1 - b1 - 2*x)*(a2 - b2 - 2*x)
 
